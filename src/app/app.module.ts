da48a0712de627c14ce96f208d8cc28500fa1c70
Santosh <santosh.v@idsnext.com>
2018-09-20 22:45:41 +0530

    code push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+
+import { DashboardComponent } from 'app/dashboard/dashboard.component';
+//Route Paths
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+
+  
+  { path: '**', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes, { useHash: true }),
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  exports: [RouterModule],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
